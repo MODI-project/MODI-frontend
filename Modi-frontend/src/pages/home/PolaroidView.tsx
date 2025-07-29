73e7b701758b670ebf57d720ee30fa57864c1018
Jung-Eon <egg9054@gmail.com>
2025-07-29 15:49:22 +0900

    Feat:더미데이터 주석 해제 및 css 개선

--- Modi-frontend/src/pages/home/PolaroidView.tsx
+++ Modi-frontend/src/pages/home/PolaroidView.tsx
@@ -145,7 +145,7 @@ export default function PolaroidView({ onSwitchView }: PolaroidViewProps) {
         </div>
         {/* ← 이 아래는 “항상 현재 일기” 것만 고정으로 보여줍니다 */}
         <div className={pageStyles.staticInfo}>
-          <div className={pageStyles.character}>
+          <div className={pageStyles.polaroidCharacter}>
             <EmotionCharacter emotion={currentDiary.emotion} />
           </div>
 
