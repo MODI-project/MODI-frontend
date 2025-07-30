7f876e76b63f086fbda50a1535863b7b2e2f0467
qkrxogmla <suk05553@naver.com>
2025-07-31 03:03:51 +0900

    feat: 자주 사용하는 키워드 컴포넌트 추가

--- Modi-frontend/src/components/common/keyword/FrequentKeywords.tsx
+++ Modi-frontend/src/components/common/keyword/FrequentKeywords.tsx
@@ -1,9 +1,29 @@
 import styles from "./FrequentKeywords.module.css";
 
+const dummyKeywords = [
+  "학교",
+  "축제",
+  "국내여행",
+  "서울",
+  "서울",
+  "학교",
+  "축제",
+  "국내여행",
+  "서울",
+  "서울",
+];
+
 const FrequentKeywords = () => {
   return (
-    <div>
-      <p className={styles.title}>키워드 사용 기록</p>
+    <div className={styles.wrapper}>
+      <p className={styles.title}>자주 사용하는 키워드</p>
+      <div className={styles.keyword_container}>
+        {dummyKeywords.map((keyword, index) => (
+          <span key={index} className={styles.keyword_chip}>
+            {keyword}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
