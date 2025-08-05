8758a678f29f931a9bbe0e7befad61aee17cc115
Jung-Eon <egg9054@gmail.com>
2025-08-05 14:49:02 +0900

    Chore: 로컬 HTTPS 개발을 위한 인증서 및 Vite 설정 추가

--- Modi-frontend/vite.config.ts
+++ Modi-frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+import fs from "fs";
 
 export default defineConfig({
   plugins: [svgr({ exportAsDefault: true }), react()],
@@ -13,6 +14,11 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
+    https: {
+      key: fs.readFileSync("./ssl/key.pem"),
+      cert: fs.readFileSync("./ssl/cert.pem"),
+    },
+    port: 5173,
   },
   define: {
     global: "globalThis",
