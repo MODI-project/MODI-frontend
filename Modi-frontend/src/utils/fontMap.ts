e0a10d71ff9417efbfccd3f33341b7832023993e
Jung-Eon <egg9054@gmail.com>
2025-08-19 00:44:33 +0900

    Feat:폰트적용 매핑 유틸 추가

---
+++ Modi-frontend/src/utils/fontMap.ts
@@ -0,0 +1,7 @@
+export const fontMap: Record<string, string> = {
+  "온글맆 박다현체": "ParkDaHyun",
+  "온글맆 류류체": "Onryuruu",
+  이서윤체: "LeeSeoYoon",
+};
+
+export const DEFAULT_FONT = "NanumSquareRoundBold";
