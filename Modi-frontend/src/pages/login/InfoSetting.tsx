9889555d45cc9e9f9db3a95a2e875a001d2265ff
joshcho <sungho020426@naver.com>
2025-09-09 18:03:30 +0900

    EDIT - CharacterContext 코드 수정

--- Modi-frontend/src/pages/login/InfoSetting.tsx
+++ Modi-frontend/src/pages/login/InfoSetting.tsx
@@ -37,7 +37,7 @@ const InitialSetting = () => {
     const loadExistingUserInfo = async () => {
       if (from === "/mypage") {
         try {
-          const userInfo = await useLoadUserInfo().userInfo();
+          const userInfo: MeResponse = await useLoadUserInfo().userInfo();
           setUserInfo(userInfo);
           setSelectedCharacter(userInfo.character);
           setCharacter(userInfo.character);
