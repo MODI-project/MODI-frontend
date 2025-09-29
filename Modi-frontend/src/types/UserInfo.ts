c6a5a5d1b56dbf564ee84706632bd8a176365f0b
joshcho <sungho020426@naver.com>
2025-09-29 09:06:11 +0900

    REFACTOR : refactor 'editUserInfo' API

--- Modi-frontend/src/types/UserInfo.ts
+++ Modi-frontend/src/types/UserInfo.ts
@@ -1,14 +1,14 @@
 // 회원가입 & 회원 정보 수정 요청 인터페이스
-export interface SignUpRequest {
+export interface MeRequest {
   nickname: string;
   character: string;
   code?: string; // Google OAuth code (선택적)
 }
 
 // 회원가입, 회원 정보 수정, 회원 정보 조회 응답 인터페이스
-export interface SignUpResponse {
+export interface MeResponse {
   userId: number;
   email: string;
   nickname: string;
-  character: string;
+  character: "momo" | "boro" | "lumi" | "zuni";
 }
