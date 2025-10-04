f3d33cd07c25b38c34ca93077175336ec972beb3
joshcho <sungho020426@naver.com>
2025-10-04 13:42:36 +0900

    각 페이지마다 라우트 보호 적용

--- Modi-frontend/src/App.tsx
+++ Modi-frontend/src/App.tsx
@@ -6,6 +6,8 @@ import { FrameTemplateProvider } from "./contexts/FrameTemplate";
 import { GeolocationProvider } from "./contexts/GeolocationContext";
 import { AlertBusProvider } from "./contexts/AlertBusContext";
 import { NotificationManagerProvider } from "./contexts/NotificationManagerContext";
+import { AuthProvider } from "./contexts/AuthContext";
+import ProtectedRoute from "./components/common/ProtectedRoute";
 
 // Pages
 import HomePage from "./pages/home/HomePage";
@@ -21,48 +23,143 @@ import DiaryKeywordPage from "./pages/diary/DiaryKeywordPage";
 import DiaryStylePage from "./pages/diary/DiaryStylePage";
 import NotificationPage from "./pages/notification/NotificationPage";
 import Setting from "./pages/setting/Setting";
-import MapMarker from "./components/map/MapMarking/MapMarker";
-import Popup from "./components/common/Popup";
 import MapSearchBar from "./components/map/SearchPlace/MapSearchBar";
 import NotificationGridPage from "./pages/notification/NotificationGridPage";
 
 function App() {
   return (
-    <CharacterProvider>
-      <DiaryDraftProvider>
-        <FrameTemplateProvider>
-          <AlertBusProvider>
-            <GeolocationProvider>
-              <NotificationManagerProvider>
-                <Routes>
-                  <Route path="/" element={<LoginPage />} />
-                  <Route path="/home" element={<HomePage />} />
-                  <Route path="/search" element={<SearchPage />} />
-                  <Route path="/emotion" element={<DiaryEmotionTag />} />
-                  <Route path="/detail" element={<DiaryWritePage />} />
-                  <Route path="/keyword" element={<DiaryKeywordPage />} />
-                  <Route path="/style" element={<DiaryStylePage />} />
-                  <Route path="/recorddetail" element={<RecordDetailPage />} />
-                  <Route path="/map" element={<MapPage />} />
-                  <Route path="/mypage" element={<MyPage />} />
-                  <Route
-                    path="/information-setting"
-                    element={<InfoSetting />}
-                  />
-                  <Route path="/notification" element={<NotificationPage />} />
-                  <Route path="/setting" element={<Setting />} />
-                  <Route path="/map-search-bar" element={<MapSearchBar />} />
-                  <Route
-                    path="/notification-grid"
-                    element={<NotificationGridPage />}
-                  />
-                </Routes>
-              </NotificationManagerProvider>
-            </GeolocationProvider>
-          </AlertBusProvider>
-        </FrameTemplateProvider>
-      </DiaryDraftProvider>
-    </CharacterProvider>
+    <AuthProvider>
+      <CharacterProvider>
+        <DiaryDraftProvider>
+          <FrameTemplateProvider>
+            <AlertBusProvider>
+              <GeolocationProvider>
+                <NotificationManagerProvider>
+                  <Routes>
+                    {/* 공개 라우트 (로그인 없이 접근 가능) */}
+                    <Route path="/" element={<LoginPage />} />
+
+                    {/* 보호된 라우트 (로그인 필요) */}
+                    <Route
+                      path="/information-setting"
+                      element={
+                        <ProtectedRoute>
+                          <InfoSetting />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/home"
+                      element={
+                        <ProtectedRoute>
+                          <HomePage />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/search"
+                      element={
+                        <ProtectedRoute>
+                          <SearchPage />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/emotion"
+                      element={
+                        <ProtectedRoute>
+                          <DiaryEmotionTag />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/detail"
+                      element={
+                        <ProtectedRoute>
+                          <DiaryWritePage />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/keyword"
+                      element={
+                        <ProtectedRoute>
+                          <DiaryKeywordPage />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/style"
+                      element={
+                        <ProtectedRoute>
+                          <DiaryStylePage />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/recorddetail"
+                      element={
+                        <ProtectedRoute>
+                          <RecordDetailPage />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/map"
+                      element={
+                        <ProtectedRoute>
+                          <MapPage />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/mypage"
+                      element={
+                        <ProtectedRoute>
+                          <MyPage />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/notification"
+                      element={
+                        <ProtectedRoute>
+                          <NotificationPage />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/setting"
+                      element={
+                        <ProtectedRoute>
+                          <Setting />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/map-search-bar"
+                      element={
+                        <ProtectedRoute>
+                          <MapSearchBar />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/notification-grid"
+                      element={
+                        <ProtectedRoute>
+                          <NotificationGridPage />
+                        </ProtectedRoute>
+                      }
+                    />
+                  </Routes>
+                </NotificationManagerProvider>
+              </GeolocationProvider>
+            </AlertBusProvider>
+          </FrameTemplateProvider>
+        </DiaryDraftProvider>
+      </CharacterProvider>
+    </AuthProvider>
   );
 }
 
